45567dd1aed0c9c1e609d14618e4b1c0f841d642
Maciej Perzankowski <perzankowski.m2001@gmail.com>
2020-06-25 18:10:51 +0200

    hydrapp

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,34 @@
 import '../scss/main.scss';
 
-// uncomment the lines below to enable PWA
-// import {registerSW} from './pwa.js';
-// registerSW();
 
-/* place your code below */
+const glassCounter = document.querySelector('.glass__counter--js');
+const buttonAdd = document.querySelector('.button__add--js');
+const buttonRemove = document.querySelector('.button__remove--js');
+const key = new Date().toLocaleString().slice(0, 10);
+const localStorageValue = localStorage.getItem(key);
 
-console.log('HELLO 🚀')
+let currentGlassCounter = 0;
+
+if (localStorageValue) {
+    currentGlassCounter = localStorageValue;
+} else {
+    localStorage.getItem(key, 0);
+}
+
+
+
+buttonAdd.addEventListener('click', () => {
+    currentGlassCounter++;
+    glassCounter.innerHTML = currentGlassCounter;
+    localStorage.setItem(key, currentGlassCounter);
+});
+
+buttonRemove.addEventListener('click', () => {
+    if (currentGlassCounter > 0) {
+        currentGlassCounter--;
+
+    }
+    glassCounter.innerHTML = currentGlassCounter;
+    localStorage.setItem(key, currentGlassCounter);
+
+});
